fa7eca74b03daed9975371ece44ad72c76a3a0a3
Ben G. Seydou Ouattara <ben.ouattara@outlook.com>
2026-06-15 12:02:50 +0000

    fix: hide settings debug menu in release

--- lib/features/settings/views/settings_screen.dart
+++ lib/features/settings/views/settings_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -228,6 +229,10 @@ class _SettingsScreenState extends ConsumerState<SettingsScreen> {
   }
 
   void _handleVersionTap() {
+    if (!kDebugMode) {
+      return;
+    }
+
     setState(() {
       _debugTapCount++;
     });
