1f79186dc5f14392127c0f4ba087d3c1502180b8
Ben G. Seydou Ouattara <ben.ouattara@outlook.com>
2026-06-17 06:56:03 +0000

    refactor: remove stale onboarding state

--- lib/features/onboarding/index.dart
+++ lib/features/onboarding/index.dart
@@ -3,10 +3,9 @@ library;
 
 export 'package:usdc_wallet/features/onboarding/models/country_data.dart';
 export 'package:usdc_wallet/features/onboarding/models/onboarding_page_data.dart';
-export 'package:usdc_wallet/features/onboarding/models/onboarding_state.dart';
 export 'package:usdc_wallet/features/onboarding/models/registration_request.dart';
 export 'package:usdc_wallet/features/onboarding/providers/onboarding_progress_provider.dart';
-export 'package:usdc_wallet/features/onboarding/providers/onboarding_provider.dart' hide OnboardingState;
+export 'package:usdc_wallet/features/onboarding/providers/onboarding_provider.dart';
 export 'package:usdc_wallet/features/onboarding/views/enhanced_onboarding_view.dart';
 export 'package:usdc_wallet/features/onboarding/views/help/deposits_guide_view.dart';
 export 'package:usdc_wallet/features/onboarding/views/help/fees_transparency_view.dart';
